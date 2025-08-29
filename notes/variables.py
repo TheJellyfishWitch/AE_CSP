88f36f3ca591a7d86d3dc9fb0671a3722eb3e50a
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-08-29 14:42:30 -0600

    varibles notes

---
+++ notes/variables.py
@@ -0,0 +1,18 @@
+# AE 7th Variables Notes
+
+#What is one way to have your program give an output?
+print("Welcome to the Variables Notes!")
+
+#examples of variables
+school = "UCAS"
+single_digit = 4
+how_much = 10
+
+print(how_much)
+
+how_much = 999999999999999999999
+
+print(how_much)
+
+name = input("What is your name?")
+
