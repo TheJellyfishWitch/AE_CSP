9968f46c433e941a62db42ec3602d77efd70b599
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-02 14:35:25 -0600

    finnished assignment

---
+++ practices/first_program.py
@@ -0,0 +1,11 @@
+#AE 7th First Program
+
+name=input("Hello! What is your name?")
+
+print("Hello", name)
+
+place_of_name=input("Where did you get it")
+
+print("Wow! thats so cool")
+
+print("I got my name", place_of_name, "there to")
---
+++ practices/variables_practice.py
@@ -0,0 +1,23 @@
+#AE 7th Variables Practice
+
+name="Addie"
+number_between_1_10="1"
+number_between_100_1000="111"
+food_for_breakfast="toast"
+favorite_color="orangy yellow"
+school_name="UCAS"
+year="2025"
+eye_color="blueish green"
+age="14"
+favorite_subject="Biology"
+
+print(name)
+print(number_between_1_10)
+print(number_between_100_1000)
+print(food_for_breakfast)
+print(favorite_color)
+print(school_name)
+print(year)
+print(eye_color)
+print(age)
+print(favorite_subject)
