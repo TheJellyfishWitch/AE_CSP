0cb494114d8e3909273eed69b2c66d8b79c8d091
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-04 14:41:48 -0600

    expressions notes

---
+++ notes/expressions.py
@@ -0,0 +1,31 @@
+# 7th Expressions notes
+
+# Algorithm
+name = input("What's your name? \n")
+print("Hello", name)
+
+# Treyson has 12 apples, he has 5 friends that he wants to give apples to. HOw many apples does each frind get?
+apples= 12
+friends= 5
+print("Each friend gets", apples/friends, "apples!")
+
+# List steps in algorithm
+    # Average age of a group of 4 people
+person_1 = 4   
+person_2 = 7
+person_3 = 8
+person_4 = 10
+total_ages =  person_1 + person_2 + person_3 + person_4
+average = total_ages/4
+print("Average age of 4 people", average)
+
+num_one = 35
+num_two = 5
+print("There are 7 different mathematical operators:")
+print("Addition(+)", num_one+num_two)
+print("Subtraction(-)", num_one-num_two)
+print("Multiplication(8)", num_one*num_two)
+print("Division(/)", num_one/num_two)
+print("Exponents(**)", num_one**num_two)
+print("Integer Division(//)", num_one//num_two)
+print("Modulo(%)", num_one%num_two)
