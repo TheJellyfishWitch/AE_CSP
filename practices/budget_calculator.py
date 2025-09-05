db447301f9095c21fd342991b65af9ae1db557b8
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-05 14:41:47 -0600

    added things

--- practices/budget_calculator.py
+++ practices/budget_calculator.py
@@ -1,3 +1,15 @@
 # AE 7th Budget Calculator
 
-print("Welcome user you have en")
+print("Welcome user this is your Financial Calculator")
+
+print("Please enter the nessasary costs:")
+income = int(input("Monthly Income\n"))
+rent = int(input("Rent/Morgage\n"))
+utilities = int(input("Utilities\n"))
+groceries = int(input("Groceries\n"))
+transportaion = int(input("Transportation\n"))
+
+spend = income - (rent + utilities + groceries + transportaion)
+percent_rent = rent/income*100
+
+print("Amount to spend:", percent_rent)
