26cde157d8dc03e8d8c28341ad803c98f47dff9a
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-06 01:37:21 +0000

    finnished budget calculator

--- practices/budget_calculator.py
+++ practices/budget_calculator.py
@@ -2,14 +2,21 @@
 
 print("Welcome user this is your Financial Calculator")
 
-print("Please enter the nessasary costs:")
+print("Please enter your monthly costs:")
 income = int(input("Monthly Income\n"))
 rent = int(input("Rent/Morgage\n"))
 utilities = int(input("Utilities\n"))
 groceries = int(input("Groceries\n"))
 transportaion = int(input("Transportation\n"))
 
-spend = income - (rent + utilities + groceries + transportaion)
-percent_rent = rent/income*100
+per_rent = rent/income*100
+per_util = utilities/income*100
+per_gro = groceries/income*100
+per_trans = transportaion/income*100
+total = income - (rent-utilities-groceries-transportaion)
 
-print("Amount to spend:", percent_rent)
+print("Your rent is $", rent, "which is", per_rent, "% of your income!")
+print("Your rent is $", utilities, "which is", per_util, "% of your income!")
+print("Your rent is $", groceries, "which is", per_gro, "% of your income!")
+print("Your rent is $", transportaion, "which is", per_trans, "% of your income!")
+print("You have $", total, "of spending each month")
