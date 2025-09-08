3d6e31533dbd32381f25fb68a60bf89f331d67ec
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-08 14:40:05 -0600

    added examples

--- notes/strings.py
+++ notes/strings.py
@@ -1,3 +1,14 @@
-# AE 7th Strings notes
+# AE 7th String notes
 
-print("I did it!")
+#Examples
+first_name = input("What's your first name?\n").strip().title()
+
+last_name = input("What's your last name?\n").strip().title()
+
+full_name = first_name + " " + last_name
+
+sentance = '               The quick brown fox jumps over the lazy dog.                          '
+print(sentance.strip())
+
+print("Welcome to my program", full_name, "!")
+
