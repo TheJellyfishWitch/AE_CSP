93adf48bdf038db8bea7a186b8409f638ba094c4
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-10 14:20:12 -0600

    finnished name decorator, changed silly sentences

---
+++ practices/name_decorator.py
@@ -0,0 +1,5 @@
+# AE 7th Name Decorator
+
+name = input("Enter name\n").title()
+
+print("|-|" + name + "|-|")
---
+++ practices/silly_sentences.py
@@ -0,0 +1,18 @@
+# AE 7th Silly Sentences
+
+noun_one = input("Enter noun\n")
+noun_two = input("Enter noun\n")
+adjective_one = input("Enter adjective\n")
+noun_three = input("Enter noun\n")
+verb_one = input("Enter verb\n")
+verb_ing_one = input("Enter verb ending in 'ing'\n")
+verb_ing_two = input("Enter verb ending in 'ing'\n")
+adjective_two = input("Enter adjective\n")
+job_one = input("Enter job\n")
+candy_one = input("Enter type of candy\n")
+adjective_three = input("Enter adjective\n")
+adjective_four = input("Enter adjective\n")
+
+print("Generating Mad-Lib...")
+
+print("On Halloween, you get to go trick-or-treating! Everyone will dress up in costumes, like a/an" + noun_one + "or a/an" + noun_two + ". This year, you decided to dress up as a/an" )
