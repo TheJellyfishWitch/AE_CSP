00eb8471eafea0628aaac02c2f6979c579755c60
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-19 14:20:44 -0600

    lists notes

---
+++ notes/lists.py
@@ -0,0 +1,7 @@
+# AE 7th Lists Notes
+
+# Including me
+siblings = ["Addie", "Joey", "John David", "Hyrum", "Jill"]
+
+# Including Ms. LaRose
+ms_larose_siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia", "Treyson", "Xavier", "Jake",]
