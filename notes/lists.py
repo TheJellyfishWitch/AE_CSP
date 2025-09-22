41f641174feb5884368239fa6b086265526bd2ec
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-22 14:05:45 -0600

    finnished lists notes

--- notes/lists.py
+++ notes/lists.py
@@ -5,3 +5,12 @@ siblings = ["Addie", "Joey", "John David", "Hyrum", "Jill"]
 
 # Including Ms. LaRose
 ms_larose_siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia", "Treyson", "Xavier", "Jake",]
+
+print(siblings[0])
+print(ms_larose_siblings)
+ms_larose_siblings[6] = "Jefferson"
+ms_larose_siblings[4] = "Victoria"
+ms_larose_siblings[0] = "Eric"
+ms_larose_siblings[7] = "Hailey"
+ms_larose_siblings.remove("Vienna")
+print(ms_larose_siblings)
