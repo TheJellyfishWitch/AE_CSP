632be6973ee2fa920c1c397e5a68fc66715a5006
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-09-23 14:17:39 -0600

    finnished fizzbuzz

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -1,2 +1,13 @@
 # AE 7th FizzBuzz
 
+number = 0
+while number <= 49:
+    number += 1
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
