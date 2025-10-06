4609fe879c80b5a1f50a73c74f9a5e337f389973
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-06 14:30:16 -0600

    finnished notes

--- notes/expressions.c
+++ notes/expressions.c
@@ -1,2 +1,21 @@
 // AE 7th Expressions Notes
+#include <stdio.h>
+#include <math.h>
 
+int main(void){
+    int year = 2025;
+    float pi = 3.14;
+    double long_pi = 3.14159265359
+
+    printf("%d\n", year);
+    printf("%d\n", 8/3); 
+    printf("%f\n", 8.0/3);
+    printf("%.2f\n", (int) pow(2, 4));
+
+    year += 1 //compound assignment operator
+    printf("%d", year)
+    year++; //incrimentor
+    printf("%d", year)
+
+    return 0;
+}
