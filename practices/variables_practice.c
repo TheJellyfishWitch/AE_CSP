2ff97e92084551ae9b82dee4305c1ad73f1f0d06
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-06 14:41:24 -0600

    not done

---
+++ practices/variables_practice.c
@@ -0,0 +1,14 @@
+// AE 7th Expressions Practice
+#include <stdio.h>
+
+int main(void){
+    char name = "Addie";
+    float num = 9;
+    int gpa = 3.65;
+    char breaky = "Toast";
+    char fav_color = "Orange";
+    char sch_name = "UCAS";
+    
+
+    return 0;
+}
