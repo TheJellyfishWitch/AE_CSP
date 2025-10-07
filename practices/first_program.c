c6dda6eaedc958a71fae9b4549c2eb28fc45bebb
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-07 04:14:11 +0000

    done

--- practices/first_program.c
+++ practices/first_program.c
@@ -1,9 +1,9 @@
 // AE 7th First Program
 #include <stdio.h>
 
-int main(void){
+int main(void) {
     char name[50];
-    printf("What is your name?");
+    printf("What is your name? ");
     scanf("%s", name);
     printf("Hello, %s! This is your first C code!\n", name);
 
