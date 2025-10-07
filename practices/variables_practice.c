60f38b7d1fab76168e776fd5933b25d0a5612617
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-07 03:52:35 +0000

    finnished

--- practices/variables_practice.c
+++ practices/variables_practice.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 int main(void){
-    char name = "Addie";
+    char name[] = "Addie";
     float num = 9;
     float gpa = 3.65;
-    char breaky = "Toast";
-    char fav_color = "Orange";
-    char sch_name = "UCAS";
-    int year = 2025;
-    char eye_color = "Blue";
+    char breaky[] = "Toast";
+    char fav_color[] = "Orange";
+    char sch_name[] = "UCAS";
+    float year = 2025;
+    char eye_color[] = "Blue";
     int age = 14;
-    char fav_subject = "Biology";
+    char fav_subject[] = "Biology";
 
-    printf("Name: %c\n", name);
+    printf("Name: %s\n", name);
     printf("A number: %.0f\n", num);
     printf("GPA: %.2f\n", gpa);
-    printf("Breakfast food: %c\n", breaky);
-    printf("Favorite color: %c\n", fav_color);
-    printf("School: %c\n", sch_name);
-    printf("Year: %d\n", year);
+    printf("Breakfast food: %s\n", breaky);
+    printf("Favorite color: %s\n", fav_color);
+    printf("School: %s\n", sch_name);
+    printf("Year: %.0f\n", year);
     printf("Eye color: %s\n", eye_color);
     printf("Age: %d\n", age);
-    printf("Favorite subject: %c\n", fav_subject);
+    printf("Favorite subject: %s\n", fav_subject);
 
     return 0;
 }
