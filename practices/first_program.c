2363ac2c9c58d5adaa9f6001b560c49f4510fcda
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-07 03:24:21 +0000

    finnished first code

---
+++ practices/first_program.c
@@ -0,0 +1,11 @@
+// AE 7th First Program
+#include <stdio.h>
+
+int main(void){
+    char name[50];
+    printf("What is your name?");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name, "this is your first C code!");
+
+    return 0;
+}
