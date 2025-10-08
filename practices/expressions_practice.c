026fc17e38b41656d235540addc0ca4325a20751
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-08 14:29:26 -0600

    finnished

--- practices/expressions_practice.c
+++ practices/expressions_practice.c
@@ -3,7 +3,33 @@
 #include <math.h>
 
 int main(void){
+
+    int return1 = 7-24/8*4+6;
+    printf("7-24/8*4+6 = %d\n", return1);
     
+    int return2 = 18/3-7+2*5;
+    printf("7-24/8*4+6 = %d\n", return2);
+
+    int return3 = 6*4/12+72/8-9;
+    printf("7-24/8*4+6 = %d\n", return3);
+
+    int return4 = (17-6/2)+4*3;
+    printf("7-24/8*4+6 = %d\n", return4);
+
+    int return5 = -2*(1*4-2/2)+(6+2-3);
+    printf("7-24/8*4+6 = %d\n", return5);
+    
+    int return6 = -1*((3-4*7)/5)-2*24/6;
+    printf("7-24/8*4+6 = %d\n", return6);
+
+    int return7 = (3*pow(5,2)/15)-(5-pow(2,2));
+    printf("7-24/8*4+6 = %d\n", return7);
+
+    int return8 = (pow(1,4)*pow(2,2)+pow(3,3))-pow(2,5)/4;
+    printf("7-24/8*4+6 = %d\n", return8);
+
+    int return9 = pow((22/2-2*5),2)+pow((4-6/6),2);
+    printf("7-24/8*4+6 = %d\n", return9);
 
     return 0;
 }
