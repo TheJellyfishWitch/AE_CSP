d17cfa14fc2fc3feb818496eda918c8b5101935a
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-09 22:38:26 +0000

    finnished

--- practices/name_decorator.c
+++ practices/name_decorator.c
@@ -1,15 +1,21 @@
-// AE 7th Silly Sentences
+// AE 7th Name Decorator
 #include <stdio.h>
 #include <string.h>
 
 int main(void){
-    char name;
+    char name[50];
     char left[] = "<<<";
     char right[] = ">>>";
+    char result[75];
+
     printf("Please enter your name:\n");
-    scanf("%c", name);
+    scanf("%s", name);
     
-    strcat(left, name);
+    strcat(result, left);
+    strcat(result, name);
+    strcat(result, right);
+
+    printf("Here is your finnished name %s", result);
 
     return 0;
 }
