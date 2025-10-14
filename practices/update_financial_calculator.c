d5862b91f10c1bc6cefaedd67785a0435e40a407
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-14 14:41:39 -0600

    not done

---
+++ practices/update_financial_calculator.c
@@ -0,0 +1,7 @@
+// AE 7th Update Financial Calculator
+#include <stdio.h>
+
+int main(void){
+
+    return 0;
+}
---
+++ practices/update_hello_world.c
@@ -0,0 +1,16 @@
+// AE 7th Update Hello World
+#include <stdio.h>
+
+void welcome(char* name){
+    printf("Hello %s\n", name);
+    scanf("%s", name);
+}
+
+int main(void){
+    welcome("Alexa");
+    welcome("Bob");
+    welcome("Jeff");
+    welcome("Jerry");
+    welcome("Nathan");
+    return 0;
+}
